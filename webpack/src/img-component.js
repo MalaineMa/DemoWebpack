8117183e59f684d1d854a06da78a19f12387ed2c
Mariem Malaine <35427636+MalaineMa@users.noreply.github.com>
2022-06-03 16:41:39 +0100

    Add files via upload

---
+++ webpack/src/img-component.js
@@ -0,0 +1,4 @@
+import imgg from'../src/img.jpg';
+const img=document.createElement("img");
+img.src=imgg;
+document.getElementById("image").appendChild(img);
---
+++ webpack/src/index.js
@@ -0,0 +1,11 @@
+
+import '../src/style.scss'
+import '../src/style.css'
+import '../src/img-component.js'
+import button from './button'
+
+document.getElementById("button").appendChild(button());
+console.log("hello world tes");
+document.addEventListener('DOMContentLoaded', function() {
+
+}, false);
